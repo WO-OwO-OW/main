61d79085c1a9f2caef04d301887a986f5cfbc046
zortum <zortum.zor@gmail.com>
2025-07-11 17:40:11 +0500

    Доработка

--- portfolio/Coffee_house/ind.js
+++ portfolio/Coffee_house/ind.js
@@ -1,6 +1,33 @@
- const images = ["1_poz.png", "2_poz.png", "3_poz.jpg"];
+window.onload = function () {
+  const images = [
+    "img/1_poz.png",
+    "img/2_poz.png",
+    "img/3_poz.jpg"
+  ];
   let index = 0;
+  let showingBg1 = true;
+
+  const bg1 = document.getElementById("bg1");
+  const bg2 = document.getElementById("bg2");
+
+  bg1.style.backgroundImage = `url('${images[index]}')`;
+
   setInterval(() => {
     index = (index + 1) % images.length;
-    document.getElementById("slider").src = images[index];
-  }, 4000); // смена каждые 4 секунды
+    const nextImage = new Image();
+    nextImage.src = images[index];
+
+    nextImage.onload = () => {
+      if (showingBg1) {
+        bg2.style.backgroundImage = `url('${nextImage.src}')`;
+        bg2.style.opacity = 1;
+        bg1.style.opacity = 0;
+      } else {
+        bg1.style.backgroundImage = `url('${nextImage.src}')`;
+        bg1.style.opacity = 1;
+        bg2.style.opacity = 0;
+      }
+      showingBg1 = !showingBg1;
+    };
+  }, 4000);
+};
